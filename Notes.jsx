911def757419e8a0755ea9d83101e16e2279ee4e
Manan Chhabra <51927605+MananChhabra@users.noreply.github.com>
2021-06-05 14:58:48 +0530

    Add files via upload

---
+++ Notes.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Notes(){
+  return(
+    <div className="note">
+      <h1>JavaScript and Reactjs</h1>
+      <p>This was an amazing bootcamp taken up by Shaurya Sinha
+         We covered everything from Scratch including Javascript React.js, HTML.</p>
+    </div>
+  )
+}
